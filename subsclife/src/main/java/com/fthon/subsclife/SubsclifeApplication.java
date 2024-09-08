f0a7f3245c5c74797d2c4158b3de1b46ccee412e
khb6997@gmail.com <khb6997@gmail.com>
2024-09-09 00:57:29 +0900

    프로젝트 생성

---
+++ subsclife/src/main/java/com/fthon/subsclife/SubsclifeApplication.java
@@ -0,0 +1,13 @@
+package com.fthon.subsclife;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubsclifeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SubsclifeApplication.class, args);
+	}
+
+}
---
+++ subsclife/src/test/java/com/fthon/subsclife/SubsclifeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fthon.subsclife;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SubsclifeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
