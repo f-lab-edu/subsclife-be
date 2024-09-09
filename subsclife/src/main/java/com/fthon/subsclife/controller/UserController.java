3fc79978f743c79dc3d5df52a8d3ad71f36d20ee
khb6997@gmail.com <khb6997@gmail.com>
2024-09-10 02:51:35 +0900

    구독 API 생성

--- subsclife/src/main/java/com/fthon/subsclife/controller/UserController.java
+++ subsclife/src/main/java/com/fthon/subsclife/controller/UserController.java
@@ -4,14 +4,12 @@ package com.fthon.subsclife.controller;
 import com.fthon.subsclife.dto.UserResponseDto;
 import com.fthon.subsclife.dto.mapper.UserMapper;
 import com.fthon.subsclife.entity.User;
+import com.fthon.subsclife.service.SubscriptionFacade;
 import com.fthon.subsclife.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/users")
@@ -20,13 +18,25 @@ public class UserController {
 
     private final UserService userService;
 
+    private final SubscriptionFacade subscriptionFacade;
+
     private final UserMapper userMapper;
 
     @PostMapping("/login")
-    public ResponseEntity<UserResponseDto> login(@RequestHeader("user_id") Long userId) {
+    public ResponseEntity<UserResponseDto> login(@RequestHeader("user-id") Long userId) {
         User user = userService.findUserById(userId);
 
         return new ResponseEntity<>(userMapper.toResponseDto(user), HttpStatus.OK);
+    }
+
+    @PostMapping("/subscribe")
+    public ResponseEntity<HttpStatus> subscribe(
+            @RequestHeader("user-id") Long userId,
+            @RequestParam("task_id") Long taskId) {
+
+        subscriptionFacade.subscribe(taskId, userId);
+
+        return ResponseEntity.ok().build();
     }
 
 }
