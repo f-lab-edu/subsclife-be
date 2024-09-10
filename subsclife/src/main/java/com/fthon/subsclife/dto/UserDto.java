cdfee995bc6e9f9dceb2799347f9e2f4bf55ff1e
khb6997@gmail.com <khb6997@gmail.com>
2024-09-10 14:03:59 +0900

    UserDto 구조 변경에 따른 mapper, test 코드 수정

---
+++ subsclife/src/main/java/com/fthon/subsclife/dto/UserDto.java
@@ -0,0 +1,49 @@
+package com.fthon.subsclife.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+public class UserDto {
+
+    @Getter
+    @NoArgsConstructor
+    public static class Response {
+        private Long id;
+        private String name;
+        private String nickname;
+
+        @Builder
+        public Response(Long id, String name, String nickname) {
+            this.id = id;
+            this.name = name;
+            this.nickname = nickname;
+        }
+    }
+
+
+    @Getter
+    @NoArgsConstructor
+    public static class SubscribedListResponse {
+
+        private Long taskId;
+        private String title;
+        private String simpleInfo;
+        private Long subscriberCount;
+        private LocalDateTime startDate;
+        private LocalDateTime endDate;
+
+        @Builder
+        public SubscribedListResponse(Long taskId, String title, String simpleInfo,
+                                      Long subscriberCount, LocalDateTime startDate, LocalDateTime endDate) {
+            this.taskId = taskId;
+            this.title = title;
+            this.simpleInfo = simpleInfo;
+            this.subscriberCount = subscriberCount;
+            this.startDate = startDate;
+            this.endDate = endDate;
+        }
+    }
+}
--- subsclife/src/main/java/com/fthon/subsclife/dto/mapper/UserMapper.java
+++ subsclife/src/main/java/com/fthon/subsclife/dto/mapper/UserMapper.java
@@ -1,14 +1,14 @@
 package com.fthon.subsclife.dto.mapper;
 
-import com.fthon.subsclife.dto.UserResponseDto;
+import com.fthon.subsclife.dto.UserDto;
 import com.fthon.subsclife.entity.User;
 import org.springframework.stereotype.Component;
 
 @Component
 public class UserMapper {
 
-    public UserResponseDto toResponseDto(User user) {
-        return UserResponseDto.builder()
+    public UserDto.Response toResponseDto(User user) {
+        return UserDto.Response.builder()
                 .id(user.getId())
                 .name(user.getName())
                 .nickname(user.getNickname())
