85360dbcb403a40f17012338ed9e567bc26dcdbc
khb6997@gmail.com <khb6997@gmail.com>
2024-09-11 00:33:59 +0900

    API 구현 및 문서 작성

--- subsclife/src/main/java/com/fthon/subsclife/controller/TaskController.java
+++ subsclife/src/main/java/com/fthon/subsclife/controller/TaskController.java
@@ -2,19 +2,19 @@ package com.fthon.subsclife.controller;
 
 
 import com.fthon.subsclife.dto.TaskDto;
+import com.fthon.subsclife.exception.ErrorInfo;
 import com.fthon.subsclife.service.TaskService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/tasks")
@@ -32,4 +32,22 @@ public class TaskController {
 
         return ResponseEntity.ok().build();
     }
+
+    @GetMapping("/{taskId}")
+    @Operation(summary = "태스크 단건 세부 조회", description = "단일 태스크의 세부 정보 조회 시 사용되는 API")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "태스크 조회 성공",
+                content = @Content(mediaType = "application/json", schema = @Schema(implementation = TaskDto.DetailResponse.class))),
+            @ApiResponse(responseCode = "401", description = "user-id가 없거나 잘못된 값이 전달되었음",
+                    content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorInfo.class))),
+            @ApiResponse(responseCode = "404", description = "존재하지 않는 리소스",
+                    content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorInfo.class)))
+    })
+    public ResponseEntity<TaskDto.DetailResponse> getDetailTaskInfo(
+            @RequestHeader("user-id") Long userId,
+            @PathVariable("taskId") Long taskId) {
+
+        return new ResponseEntity<>(taskService.getTaskDetail(taskId), HttpStatus.OK);
+    }
+
 }
